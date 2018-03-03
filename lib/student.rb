448ce47379ca1854d921faba79623c5b256f24b3
Michael Pope <popemray1986@gmail.com>
2018-03-03 22:44:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,8 +49,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new(id, name, grade)
-    student.id = row[0]
+    student = self.new(name, grade)
     student.name = row[1]
     student.grade = row[2]
     student
